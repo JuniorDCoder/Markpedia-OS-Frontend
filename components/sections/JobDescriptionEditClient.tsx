bc9a4cead99dd7868afae925ecb5bbdb2ec860df
Junior D'Coder <119924067+JuniorDCoder@users.noreply.github.com>
2025-12-20 12:39:21 +0100

    Remove unnecessary closing bracket

--- components/sections/JobDescriptionEditClient.tsx
+++ components/sections/JobDescriptionEditClient.tsx
@@ -49,8 +49,6 @@ export default function JobDescriptionEditClient({ jobDescriptionId, initialJobD
         status: 'Draft',
     });
 
-});
-
 // Check if user is CEO
 useEffect(() => {
     if (user && user.role !== 'CEO') {
